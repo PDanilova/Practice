bcd055658484be0a887ada111279ba6d10678ba7
Polina Danilova <danilovapol@gmail.com>
2019-06-01 21:25:55 +0500

    Решение задачи №3 (учебник 60 в).)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,46 @@ namespace ConsoleApp24
 {
     class Program
     {
+        static double Enter()
+        {
+            bool check;
+            double coord;
+
+            do
+            {
+                check = Double.TryParse(Console.ReadLine(), out coord);
+                if (!check) Console.WriteLine("\nОшибка! Введите рациональное число.");
+            } while (!check);
+
+            return coord;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("\n     Задание №3");
+            Console.Write("\nВам нужно ввести координаты точки.   \n   Введите х:  ");
+            double x = Enter();
+            Console.Write("   Введите y: ");
+            double y = Enter();
+
+            bool condition = false;
+            double u = 0;
+
+            if ((y <= 1 - x * x) && x * x + Math.Pow((y - 1), 2) <= 1) condition = true; //проверка принадлежности к заштрихованной области
+
+            switch (condition)
+            {
+                case true: //если точка входит в закрашенную область
+                    u = x - y;
+                    break;
+
+                case false: //если точка не входит в закрашенную область
+                    u = x * y + 7;
+                    break;
+            }
+
+            Console.WriteLine("\n   U = " + u);
+            Console.ReadKey();
         }
     }
 }
