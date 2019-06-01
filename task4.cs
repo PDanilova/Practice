ffafc22f7aa893a2d352db89b060c537901bf656
Polina Danilova <danilovapol@gmail.com>
2019-06-02 00:43:51 +0500

    Задание №4 (119г)

--- task4.cs
+++ task4.cs
@@ -8,8 +8,46 @@ namespace task4
 {
     class Program
     {
+        static double fact (double number)
+        {
+            if (number < 0)
+                return 0;
+            if (number == 0)
+                return 1;
+            else
+                return number * fact(number - 1);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("\n   Задание №4. Вариант 119г");
+
+            Console.WriteLine("\n   Введите точность вычислений: ");
+
+            double epsilon;
+            bool check;
+
+            do
+            {
+                check = double.TryParse(Console.ReadLine(), out epsilon);
+                if (!check || epsilon <= 0) Console.WriteLine("\nОшибка! Введите положительное число");
+            } while (!check || epsilon <= 0);
+
+            double sum = 0;
+            int i = 1;
+            double sl;
+
+            while (true)
+            {
+                sl = Math.Pow(-2, i) / fact(i);
+                if (Math.Abs(sl) < epsilon) break;
+                sum += sl;
+                i++;
+            }
+
+            Console.WriteLine("\nСумма ряда: " + sum);
+            Console.ReadKey();
+
         }
     }
 }
