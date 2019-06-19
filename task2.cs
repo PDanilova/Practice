d6f4671b06dc9735e6c348e6f38cb389285bf6c6
Polina Danilova <danilovapol@gmail.com>
2019-06-19 14:52:34 +0500

    задание №2 (олимпиадная задача №458)

--- task2.cs
+++ task2.cs
@@ -8,55 +8,50 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            string key;
-            string str;
-            string text;
+            string[] vvod = Console.ReadLine().Split();
+            string[] key = null;
 
-            StreamReader sr = new StreamReader("input.txt", Encoding.GetEncoding(1251));
-
-                str = sr.ReadLine(); //считываем количество строк
-                key = sr.ReadLine(); //считываем ключ
-                text = sr.ReadLine(); //считываем сообщение
+            int str = int.Parse(vvod[0]);
             
-            bool ok = Int32.TryParse(str, out int strings);
-
-            string[] kkey = key.Split(new char[] { ' ' }); //переносим ключ в массив
-
-            int[] mas = new int[kkey.Length]; //массив из цифр ключа
+            key = Console.ReadLine().Split();
+            
+            string text = Console.ReadLine(); //считываем сообщение
+            
+            int[] mas = new int[key.Length]; //массив из цифр ключа
             int j = 0;
 
-            foreach (string ch in kkey) //переносим ключ в массив из цифр
+            foreach (string ch in key) //переносим ключ в массив из цифр
             {
-                ok = Int32.TryParse(ch, out mas[j]);
+                bool ok = Int32.TryParse(ch, out mas[j]);
                 j++;
             }
 
-            char[][] table = new char[strings][];
+            char[][] table = new char[str][];
 
-            int remainder = text.Length % strings; //количество столбцов в последней строке
+            int remainder = text.Length % str; //количество строк в последнем столбце
 
-            double result = text.Length / strings;
+            int result = text.Length / str;
 
-            int columns = Convert.ToInt32(Math.Ceiling(result)); //число основных столбцов
+            int columns;
 
-            int k;
-
-            if (remainder == 0) k = strings;
-            else k = strings - 1;
+            if (remainder != 0) columns = result + 1;
+            else columns = result;
 
             //определяем количество столбцов в каждой строке 
-            if (!(remainder == 0))
+            if (remainder != 0)
             {
-                for (int i = 0; i < strings - 1; i++)
+                for (int i = 0; i < remainder; i++) 
                 {
                     table[i] = new char[columns];
                 }
-
-                table[strings - 1] = new char[remainder];
+                for (int i = remainder; i < str; i++)
+                {
+                    table[i] = new char[columns - 1];
+                }
             } 
             else
             {
-                for (int i = 0; i < strings; i++)
+                for (int i = 0; i < str; i++)
                 {
                     table[i] = new char[columns];
                 }
@@ -65,21 +60,28 @@ namespace task2
             int charOfText = 0;
 
             //заполнение таблицы
-            for (int i = 1; i <= strings; i++)
+            for (int i = 1; i <= str; i++)
             {
                 int a = mas[i - 1];
 
-                if (remainder != 0 && a == strings - 1) //если это последняя строка с неполным кол-вом столбцов
+                if (remainder != 0 && a <= remainder) //если это длинные строки
                 {
-                    for (int n = 0; n < remainder; n++)
+                    for (int n = 0; n < columns; n++)
                     {
-                        table[strings - 1][n] = text[charOfText];
+                        table[a - 1][n] = text[charOfText];
                         charOfText++;
                     }
                 }
-                else
+                else if (remainder == 0)
                 {
                     for (int n = 0; n < columns; n++)
+                    {
+                        table[a - 1][n] = text[charOfText];
+                        charOfText++;
+                    }
+                } else
+                {
+                    for (int n = 0; n < columns - 1; n++)
                     {
                         table[a - 1][n] = text[charOfText];
                         charOfText++;
@@ -91,32 +93,30 @@ namespace task2
             int number = 0;
 
             //перевод полученной таблицы в строку
-            if (remainder == 0) //если количество столбцов в последней строке полное
+            if (remainder == 0) //если количество строк с столбцах одинаковое
             {
                 for (int i = 0; i< columns; i++)
                 {
-                    for (int n = 0; n < strings; n++)
+                    for (int n = 0; n < str; n++)
                     {
                         newText[number] = table[n][i];
                         number++;
-                        Console.Write(table[n][i]);
                     }
                 }
             }
             else
             {
-                for (int i = 0; i < remainder; i++)
+                for (int i = 0; i < columns - 1; i++)
                 {
-                    for (int n = 0; n < strings; n++)
+                    for (int n = 0; n < str; n++)
                     {
                         newText[number] = table[n][i];
                         number++;
-                        Console.Write(table[n][i]);
                     }
                 }
-                for (int i = remainder; i < columns; i++)
+                for (int i = columns - 1; i < columns; i++)
                 {
-                    for (int n = 0; n < strings - 1; n++)
+                    for (int n = 0; n < remainder; n++)
                     {
                         newText[number] = table[n][i];
                         number++;
@@ -124,13 +124,9 @@ namespace task2
                 }
             }
             
-            StreamWriter sw = new StreamWriter("output.txt");
-            sw.WriteLine(newText);
+            string res = new string(newText);
+            Console.Write(res);
             
-            sr.Close();
-            sw.Close();
-
-            Console.ReadKey();
         }
     }
 }
